5f1f38be11b7032aedeedceac206e7bbd183234e
Lita Gratrix <lgratrix@mail.techempower.com>
2012-02-17 11:37:51 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '155f1032ea497f3c9c596b3cbd9be48f4175e352f60280258321df924ec83f2e48615b0a336ddaa0171d0546872ce7bef310692f1e586e6d3b9553020f491b59'
